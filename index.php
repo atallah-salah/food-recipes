fb1cb678d2bac4e1a5ef888c4c3cc2407bc15f30
atallah-salah <atallah_salah@outlook.com>
2018-01-17 20:20:38 -0500

    require files

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
-echo"<h1>test</h1>";
 require "server-side/config.php";
 require "server-side/createTables.php";
+require "client-side/header.php";
+require "client-side/navbar.php";
+require "client-side/login.php";
+require "client-side/signup.php";
+  // switch($go){
+  // 	case 'login':
+  // 		include ('client-side/login.php');
+  //         break;
+  //         case 'signup':
+  // 		include ('client-side/signup.php');
+  //         break;
+  // 	default:
+  // 		include ('File4H/no_page.php');
+  //        	break;
+  // }
  ?>
