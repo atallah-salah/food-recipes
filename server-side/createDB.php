f9fcb459eb88efbfa49850d0220d928dd315cc5b
atallah-salah <atallah_salah@outlook.com>
2018-01-19 19:44:38 -0500

    create database

--- server-side/createDB.php
+++ server-side/createDB.php
@@ -1,3 +1,10 @@
-// Create database
-// $createDB = "CREATE DATABASE foodRecipes";
-// mysqli_query($connect,$createDB);
+<?php
+  // connect to server
+  $server = "localhost";
+  $username = "root";
+  $password = "";
+  $connectServer = new mysqli($server, $username, $password);
+  // craete foodRecipes database
+  $createDB="CREATE DATABASE IF NOT EXISTS foodRecipes";
+  mysqli_query($connectServer,$createDB);
+ ?>
