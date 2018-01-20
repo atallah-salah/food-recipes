4908af831c20eda56a2299a41feac83d5cf41288
atallah-salah <atallah_salah@outlook.com>
2018-01-19 19:46:14 -0500

    make index.php

--- index.php
+++ index.php
@@ -1,19 +1,11 @@
 <?php
-require "server-side/config.php";
-require "server-side/createTables.php";
-require "client-side/header.php";
-require "client-side/navbar.php";
-require "client-side/login.php";
-require "client-side/signup.php";
-  // switch($go){
-  // 	case 'login':
-  // 		include ('client-side/login.php');
-  //         break;
-  //         case 'signup':
-  // 		include ('client-side/signup.php');
-  //         break;
-  // 	default:
-  // 		include ('File4H/no_page.php');
-  //        	break;
-  // }
+  // require files
+  require "server-side/createDB.php";
+  require "server-side/config.php";
+  require "server-side/createTables.php";
+  require 'server-side/insertData.php';
+  require "client-side/header.php";
+  require "client-side/navbar.php";
+  require "client-side/login.php";
+  require "client-side/signup.php";
  ?>
